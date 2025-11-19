6c076fc956e5b030aaedda5acf94824c746467e4
Onur Karagür <116795501+Onurk77@users.noreply.github.com>
2025-11-20 01:14:58 +0300

    Created dataloader.py

---
+++ src/dataloader.py
@@ -0,0 +1,13 @@
+from torch.utils.data import DataLoader
+from dataset import GTZANDataset
+
+def get_loaders(batch_size=32):
+    train_ds = GTZANDataset("data/train")
+    val_ds = GTZANDataset("val/train")
+    test_ds = GTZANDataset("test/train")
+
+    train_loader = DataLoader(train_ds, batch_size=batch_size, shuffle=True)
+    val_loader = DataLoader(val_ds, batch_size=batch_size, shuffle=False)
+    test_loader = DataLoader(test_ds, batch_size=batch_size, shuffle=False)
+
+    return train_loader, val_loader, test_loader
