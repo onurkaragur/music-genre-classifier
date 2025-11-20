baec468bbc8b4f49d214e62db61970f84b21f9f0
Onur Karagür <116795501+Onurk77@users.noreply.github.com>
2025-11-20 12:52:38 +0300

    Created model and updated README

---
+++ src/model.py
@@ -0,0 +1,26 @@
+import torch.nn as nn
+
+class AudioNN(nn.Module):
+    def __init__(self, num_classes=10):
+        super().__init__()
+        self.net = nn.Sequential(
+            nn.Conv2d(1, 16, 3, stride=1, padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(2),
+
+            nn.Conv2d(16, 32, 3, padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(2),
+
+            nn.Conv2d(32, 64, 3, padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(2),
+
+            nn.Flatten(),
+            nn.Linear(64 * 8 * 10, 128),
+            nn.ReLU(),
+            nn.Linear(128, num_classes)
+        )
+
+    def forward(self, x):
+        return self.net(x)
