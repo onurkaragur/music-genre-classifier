f70f781dd4db99b4f71b485855bc1791830431bf
Onur Karagür <116795501+Onurk77@users.noreply.github.com>
2025-11-22 12:10:36 +0300

    Project finalized

--- src/train.py
+++ src/train.py
@@ -61,7 +61,7 @@ if __name__ == "__main__":
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     print(f"Using device: {device}")
     
-    num_epochs = 20
+    num_epochs = 40
     batch_size = 32
     learning_rate = 0.001
     
